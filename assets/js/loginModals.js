8c08a33a66c6a39bdc8f4df7d6fd65d05703e1f4
Mike <mike_wiii@hotmail.com>
2021-03-14 14:40:56 -0600

    Usuario reciente e iniciar sesion terminados

--- assets/js/loginModals.js
+++ assets/js/loginModals.js
@@ -1,8 +1,33 @@
-const addUserModal = $('#loginModal');
-const addUser = $('#login');
+//Recent user modal
+
+const recentUserModal = $('#recentUserModal');
+const login = $('#login');
+
+login.click(()=>{
+	recentUserModal.modal({
+		fadeDuration: 100,
+		fadeDelay: 0.5
+	});
+});
+
+
+//Add user modal
+const addUserModal = $('#addUserModal');
+const addUser = $('#addUser');
 
 addUser.click(()=>{
 	addUserModal.modal({
+		fadeDuration: 100,
+		fadeDelay: 0.5
+	});
+});
+
+//Sign in
+const signInModal = $('#signInModal');
+const singIn = $('#signIn');
+
+singIn.click(()=>{
+	signInModal.modal({
 		fadeDuration: 100,
 		fadeDelay: 0.5
 	});
